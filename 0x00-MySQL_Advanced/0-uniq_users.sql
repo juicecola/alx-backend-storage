3c4966038e33a7e3b04dfa3af79c98c16d64d0b6
Eric Munyi <irerieric@gmail.com>
2023-07-13 06:26:41 +0000

    SQL script that creates a table users

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,11 +1,11 @@
---Create table only if it doesn't exist
-CREATE TABLE IF NOT EXIT users (
-	--Unique identifier for each user
-	id INTEGER PRIMARY KEY AUTO_INCREMENT,
-
-	--User email (max 255 char)
-	email VARCHAR(255) NOT NULL UNIQUE,
-
-	--User name (max 255 char)
-	name VARCHAR(255)
+-- --Create table only if it doesn't exist
+CREATE TABLE IF NOT EXISTS users (
+    --Unique identifier for each user
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    
+    -- User email (max 255 char)
+    email VARCHAR(255) NOT NULL UNIQUE,
+    
+    --Username (max 255 char)
+    name VARCHAR(255)
 );
